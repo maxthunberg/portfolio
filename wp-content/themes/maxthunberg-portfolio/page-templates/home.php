07432db53a72bb04f05cd5bb4457f870ef15e3c4
Max Thunberg <MaxThunberg@Maxs-MacBook-Pro.local>
2017-10-08 22:55:00 +0200

    Proper SASS structure

--- wp-content/themes/maxthunberg-portfolio/page-templates/home.php
+++ wp-content/themes/maxthunberg-portfolio/page-templates/home.php
@@ -13,8 +13,8 @@ Template Name: Home
 
   <div class="card six">
     <div class="card-text--white">
-      <h4>Alfa Laval</h4>
-      <h2>Landing<br />Page</h2>
+      <h4>SCHOOL</h4>
+      <h2>Icon set:<br />Yrgo</h2>
     </div>
 
     <div class="card-img">
@@ -37,8 +37,8 @@ Template Name: Home
 
   <div class="card two">
     <div class="card-text--white">
-      <h4>Alfa Laval</h4>
-      <h2>Landing<br />Page</h2>
+      <h4>AGROWTH</h4>
+      <h2>Branding<br />Project</h2>
     </div>
 
     <div class="card-img">
@@ -82,6 +82,34 @@ Template Name: Home
       </div>
 
     </div> -->
+
+</div>
+
+<!-- arrow buttons-->
+
+<div class="button-arrows">
+  <div class="button-arrow button-arrow--left">
+    <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/icons/icn-arrow--left.svg" />
+  </div>
+
+  <div class="button-arrows--updown">
+
+  <div class="button-arrow button-arrow--up">
+    <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/icons/icn-arrow--up.svg" />
+  </div>
+
+    <div class="button-arrow button-arrow--down">
+      <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/icons/icn-arrow--down.svg" />
+    </div>
+
+  </div>
+
+
+  <div class="button-arrow button-arrow--right">
+    <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/icons/icn-arrow--right.svg" />
+  </div>
+
+
 
 </div>
 
